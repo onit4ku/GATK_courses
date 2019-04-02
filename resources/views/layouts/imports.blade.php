dca93a898f611f02dea2bc527c171c6d4720c659
Rubén García Serrano <onitaku@gmail.com>
2019-04-02 10:41:26 +0200

    fontawesome

--- resources/views/layouts/imports.blade.php
+++ resources/views/layouts/imports.blade.php
@@ -10,3 +10,5 @@
 <!-- bootstrap-validator -->
 <script type="text/javascript"
     src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/js/bootstrapValidator.min.js"></script>
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
