c55243ce83afe191f6c9a70597c8183d91cbd649
Rubén García Serrano <onitaku@gmail.com>
2019-04-02 10:41:34 +0200

    px to rem

--- resources/views/layouts/heads.blade.php
+++ resources/views/layouts/heads.blade.php
@@ -11,7 +11,7 @@ body {
 
 .logoGATK img {
     float: left;
-    margin-left: 10px
+    margin-left: 1rem
 }
 
 .clear {
@@ -19,60 +19,60 @@ body {
 }
 
 .form-control-feedback {
-    padding-right: 5px;
-    padding-top: 5px
+    padding-right: 0.5rem;
+    padding-top: 0.5rem
 }
 
 .form-style-10 {
-    width: 650px;
-    padding: 30px;
-    margin: 40px auto;
+    width: 65rem;
+    padding: 3rem;
+    margin: 4rem auto;
     background: #FFF;
-    border-radius: 10px;
-    -webkit-border-radius: 10px;
-    -moz-border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, .13);
-    -moz-box-shadow: 0 0 10px rgba(0, 0, 0, .13);
-    -webkit-box-shadow: 0 0 10px rgba(0, 0, 0, .13)
+    border-radius: 1rem;
+    -webkit-border-radius: 1rem;
+    -moz-border-radius: 1rem;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, .13);
+    -moz-box-shadow: 0 0 1rem rgba(0, 0, 0, .13);
+    -webkit-box-shadow: 0 0 1rem rgba(0, 0, 0, .13)
 }
 
 .form-style-10 .inner-wrap {
-    padding: 30px;
+    padding: 3rem;
     background: #F8F8F8;
-    border-radius: 6px;
-    margin-bottom: 15px
+    border-radius: 0.6rem;
+    margin-bottom: 1.5rem
 }
 
 .form-style-10 h1 {
     background: #2A88AD;
-    padding: 20px 30px 15px;
-    margin: -30px -30px 30px;
-    border-radius: 10px 10px 0 0;
-    -webkit-border-radius: 10px 10px 0 0;
-    -moz-border-radius: 10px 10px 0 0;
+    padding: 2rem 3rem 1.5rem;
+    margin: -3rem -3rem 3rem;
+    border-radius: 1rem 1rem 0 0;
+    -webkit-border-radius: 1rem 1rem 0 0;
+    -moz-border-radius: 1rem 1rem 0 0;
     color: #fff;
-    text-shadow: 1px 1px 3px rgba(0, 0, 0, .12);
-    font: 400 30px Bitter, serif;
-    -moz-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, .17);
-    -webkit-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, .17);
-    box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, .17);
-    border: 1px solid #257C9E
+    text-shadow: 0.1rem 0.1rem 0.3rem rgba(0, 0, 0, .12);
+    font: 400 3rem Bitter, serif;
+    -moz-box-shadow: inset 0 0.2rem 0.2rem 0 rgba(255, 255, 255, .17);
+    -webkit-box-shadow: inset 0 0.2rem 0.2rem 0 rgba(255, 255, 255, .17);
+    box-shadow: inset 0 0.2rem 0.2rem 0 rgba(255, 255, 255, .17);
+    border: 0.1rem solid #257C9E
 }
 
 .form-style-10 h1>span,
 .form-style-10 label {
-    font: 14px Arial, Helvetica, sans-serif;
+    font: 1.4rem Arial, Helvetica, sans-serif;
     display: block
 }
 
 .form-style-10 h1>span {
-    margin-top: 2px
+    margin-top: 0.2rem
 }
 
 .form-style-10 label {
     color: #726e6e;
     font-weight: 600;
-    margin-bottom: 15px
+    margin-bottom: 1.5rem
 }
 
 .form-style-10 input[type=text],
@@ -91,67 +91,67 @@ body {
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     width: 100%;
-    padding: 8px;
-    border-radius: 6px;
-    -webkit-border-radius: 6px;
-    -moz-border-radius: 6px;
-    border: 2px solid #fff;
-    box-shadow: inset 0 1px 1px rgba(0, 0, 0, .33);
-    -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .33);
-    -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .33)
+    padding: 0.8rem;
+    border-radius: 0.6rem;
+    -webkit-border-radius: 0.6rem;
+    -moz-border-radius: 0.6rem;
+    border: 0.2rem solid #fff;
+    box-shadow: inset 0 0.1rem 0.1rem rgba(0, 0, 0, .33);
+    -moz-box-shadow: inset 0 0.1rem 0.1rem rgba(0, 0, 0, .33);
+    -webkit-box-shadow: inset 0 0.1rem 0.1rem rgba(0, 0, 0, .33)
 }
 
 .form-style-10 .section {
-    font: 400 20px Bitter, serif;
+    font: 400 2rem Bitter, serif;
     color: #2A88AD;
-    margin-bottom: 5px
+    margin-bottom: 0.5rem
 }
 
 .form-style-10 .section span {
     background: #2A88AD;
-    padding: 5px 10px;
+    padding: 0.5rem 1rem;
     position: absolute;
     border-radius: 50%;
     -webkit-border-radius: 50%;
     -moz-border-radius: 50%;
-    border: 4px solid #fff;
-    font-size: 14px;
-    margin-left: -45px;
+    border: 0.4rem solid #fff;
+    font-size: 1.4rem;
+    margin-left: -4.5rem;
     color: #fff;
-    margin-top: -3px
+    margin-top: -0.3rem
 }
 
 .form-style-10 input[type=button],
 .form-style-10 input[type=submit] {
     background: #2A88AD;
-    padding: 8px 20px;
-    border-radius: 5px;
-    -webkit-border-radius: 5px;
-    -moz-border-radius: 5px;
+    padding: 0.8rem 2.0rem;
+    border-radius: 0.5rem;
+    -webkit-border-radius: 0.5rem;
+    -moz-border-radius: 0.5rem;
     color: #fff;
-    text-shadow: 1px 1px 3px rgba(0, 0, 0, .12);
-    font: 400 30px Bitter, serif;
-    -moz-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, .17);
-    -webkit-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, .17);
-    box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, .17);
-    border: 1px solid #257C9E;
-    font-size: 15px
+    text-shadow: 0.1rem 0.1rem 0.3rem rgba(0, 0, 0, .12);
+    font: 400 3.0rem Bitter, serif;
+    -moz-box-shadow: inset 0 0.2rem 0.2rem 0 rgba(255, 255, 255, .17);
+    -webkit-box-shadow: inset 0 0.2rem 0.2rem 0 rgba(255, 255, 255, .17);
+    box-shadow: inset 0 0.2rem 0.2rem 0 rgba(255, 255, 255, .17);
+    border: 0.1rem solid #257C9E;
+    font-size: 1.5rem
 }
 
 .form-style-10 input[type=button]:hover,
 .form-style-10 input[type=submit]:hover {
     background: #2A6881;
-    -moz-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, .28);
-    -webkit-box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, .28);
-    box-shadow: inset 0 2px 2px 0 rgba(255, 255, 255, .28)
+    -moz-box-shadow: inset 0 0.2rem 0.2rem 0 rgba(255, 255, 255, .28);
+    -webkit-box-shadow: inset 0 0.2rem 0.2rem 0 rgba(255, 255, 255, .28);
+    box-shadow: inset 0 0.2rem 0.2rem 0 rgba(255, 255, 255, .28)
 }
 
 .form-style-10 .privacy-policy {
     float: right;
-    width: 250px;
-    font: 12px Arial, Helvetica, sans-serif;
+    width: 25.0rem;
+    font: 1.2rem Arial, Helvetica, sans-serif;
     color: #4D4D4D;
-    margin-top: 10px;
+    margin-top: 1rem;
     text-align: right
 }
 </style>
