ea0b25e2c0736ac4727662991dc1ed1afaaa852b
Rubén García Serrano <onitaku@gmail.com>
2019-04-11 14:14:54 +0200

    start page

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/upload', 'UploadController@uploadForm');
 Route::post('/upload', 'UploadController@uploadSubmit');
