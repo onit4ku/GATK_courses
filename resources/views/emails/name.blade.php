340581358c1ca9b054f647e5492b08121b592163
Rubén García Serrano <onitaku@gmail.com>
2019-07-01 14:43:08 +0200

    template 2.0

--- resources/views/emails/name.blade.php
+++ resources/views/emails/name.blade.php
@@ -11,52 +11,201 @@ if ($gala == "withDinner") {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<meta content="width=device-width, initial-scale=1.0" name="viewport">
-	<meta content="ie=edge" http-equiv="X-UA-Compatible">
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 	<title>GATK registration email</title>
+	<style type="text/css">
+	   @media only screen and (max-width: 550px), screen and (max-device-width: 550px) {
+	    body[yahoo] .hide {
+	      display: none!important;
+	    }
+
+	    body[yahoo] .buttonwrapper {
+	      background-color: transparent!important;
+	    }
+
+	    body[yahoo] .button {
+	      padding: 0px!important;
+	    }
+
+	    body[yahoo] .button a {
+	      background-color: #e05443;
+	      padding: 15px 15px 13px!important;
+	    }
+
+	    body[yahoo] .unsubscribe {
+	      display: block;
+	      margin-top: 20px;
+	      padding: 10px 50px;
+	      background: #2f3942;
+	      border-radius: 5px;
+	      text-decoration: none!important;
+	      font-weight: bold;
+	    }
+	   }
+	</style>
 </head>
-<body style=" @import url('https://fonts.googleapis.com/css?family=Lato&display=swap'); background-color: #e0e0e0; color: #333; font-family:Lato; img{ -ms-interpolation-mode:bicubic; } body{ -webkit-text-size-adjust:100%; } h2{ color:#0066CC !important; }">
-	<div style="margin-left: 20px">
-		<p style="font-size:20px;">Dear <b>{{ $name }},</b></p>
-		<p>Thank you for submitting your pre-registration to attend the GATK Workshop 2019 in Granada, Spain. Your pre-registration has now been saved in our database.</p><br>
-		<p>Please, in order to prepare an invoice we need you to provide us with your <b>tax information:</b></p><b></b>
-		<div style="margin-left: 20px; align-content: center; padding: 15px; background-color:white; border-radius: 25px; background: #ffffff; padding: 20px; width: 480px;">
-			<p><b>Full name</b></p>
-			<p><b>NIE / CIF</b></p>
-			<p><b>Organization, institution.</b></p>
-			<p><b>Billing Address</b></p>
-		</div>
-		<p>In order to address the invoice <b>to your institution</b>, please indicate the detailed information for your billing, otherwise we will address the invoice to you.</p><!-- <p>Should you need an invoice, please inform us of the correct invoicing address, (if different from the address above and the VAT number if applicable, that information needs to be received within 24 hours). Invoices will become available online two weeks upon receipt of remittance. </p> -->
-		<p>Finally, you need to make the deposit and send the proof of income to the following email:<br>
-		<b>secretariatecnica-clinbioinfosspa.fps@juntadeandalucia.es</b></p><br>
-		<p><i>You have chosen the following reservation in your registration:</i></p>
-		<ul>
-			<li><b>Full name:</b> {{ $name }}.</li>
-			<li><b>Position:</b> {{ $institution }}.</li>
-			<li><b>Organization, institution:</b> {{ $position }}.</li>
-			<li><b>Reservation plan:</b> {{ $result }}.</li>
-		</ul>
-		<p>If you have checked the box of the reservation of the visit to the Alhambra, you should keep in mind that it is <b>only a reservation.</b></p>
-		<p>The visit of the Alhambra <b>must be paid in the location of the course</b>, it's not possible to include the fee in the transference.</p><br>
-		<div style=" margin-left: 20px; align-content: center; padding: 15px; background-color:white; border-radius: 25px; background: #ffffff; padding: 20px; width: 815px;">
-			<h3>Payment via Bank transfer</h3>
-			<h4>Bank Account Details</h4>
-			<h4></h4>
-			<p><b>Account holder:</b> Fundación Progreso y Salud</p>
-			<p><b>IBAN:</b> ES1101825566790011503217</p>
-			<p><b>BIC/SWIFT:</b> BBVAESMMXXX</p>
-			<p><b>Bank:</b> BBVA</p>
-			<p>PLEASE MENTION YOUR NAME FOLLOWED BY "GATK Workshop 2019" IN YOUR PAYMENT REFERENCE.</p>
-		</div><br>
-		<p>Once the payment has been registered in our system you will receive a confirmation email.</p><br>
-		<p style="/* margin: 20px; */"><b>Best regards,<br>
-		Clinical Bioinformatics Area team.</b></p>
-		<div style="padding: 20px;background-color: #b9b9b9;color: white;align-items: center;">
-			<a href="http://www.clinbioinfosspa.es" rel="noopener noreferrer" target="_blank"><img alt="cba logo" src="http://www.clinbioinfosspa.es/files/image/logo-CBA.png" style="height: 75px;"></a> <a href="https://software.broadinstitute.org/gatk/" rel="noopener noreferrer" target="_blank"><img alt="gatk logo" src="http://www.clinbioinfosspa.es/files/image/logoGATK1.png" style="height: 75px;"></a><a href="https://www.broadinstitute.org/" rel="noopener noreferrer" target="_blank"><img alt="broad logo" src="http://www.clinbioinfosspa.es/files/image/logoGATK.png" style="height: 70px;"></a>
-		</div>
-	</div>
+<body bgcolor="#F6F8F1" style="margin: 0; padding: 0; min-width: 100%;">
+	<table bgcolor="#F6F8F1" border="0" cellpadding="0" cellspacing="0" width="100%">
+		<tr>
+			<td>
+				<!--[if (gte mso 9)|(IE)]>
+      <table width="600" align="center" cellpadding="0" cellspacing="0" border="0">
+        <tr>
+          <td>
+    <![endif]-->
+				<table align="center" bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="0" class="content" style="width: 100%; max-width: 600px;" width="100%">
+					<tr>
+						<td bgcolor="#1B63C0" class="header" style="padding: 40px 30px 20px 30px;">
+							<table align="left" border="0" cellpadding="0" cellspacing="0" width="70">
+								<tr>
+									<td height="70" style="padding: 0 20px 20px 0;"><img alt="" border="0" class="fix" height="70" src="http://clinbioinfosspa.es/files/image/fps-blanco.svg" style="height: auto;" width="70"></td>
+								</tr>
+							</table><!--[if (gte mso 9)|(IE)]>
+            <table width="425" align="left" cellpadding="0" cellspacing="0" border="0">
+              <tr>
+                <td>
+          <![endif]-->
+							<table align="left" border="0" cellpadding="0" cellspacing="0" class="col425" style="width: 100%; max-width: 425px;">
+								<tr>
+									<td height="70">
+										<table border="0" cellpadding="0" cellspacing="0" width="100%">
+											<tr>
+												<td class="subhead" style="font-size: 15px; color: #ffffff; font-family: sans-serif; letter-spacing: 10px; padding: 0 0 0 3px;">Pre-registration Email</td>
+											</tr>
+											<tr>
+												<td class="h1" style="color: #ffffff; font-family: sans-serif; font-size: 33px; line-height: 38px; font-weight: bold; padding: 5px 0 0 0;">Dear {{ $name }}</td>
+											</tr>
+										</table>
+									</td>
+								</tr>
+							</table><!--[if (gte mso 9)|(IE)]>
+                </td>
+              </tr>
+          </table>
+          <![endif]-->
+						</td>
+					</tr>
+					<tr>
+						<td class="innerpadding borderbottom" style="padding: 30px 30px 30px 30px; border-bottom: 1px solid #f2eeed;">
+							<table border="0" cellpadding="0" cellspacing="0" width="100%">
+								<tr>
+									<td class="h2" style="color: #153643; font-family: sans-serif; padding: 0 0 15px 0; font-size: 24px; line-height: 28px; font-weight: bold;">Your pre-registration has now been saved in our database.</td>
+								</tr>
+								<tr>
+									<td class="bodycopy" style="color: #153643; font-family: sans-serif; font-size: 16px; line-height: 22px;">Thank you for submitting your pre-registration to attend the <b>GATK Workshop 2019</b> in Granada, Spain.</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+					<tr>
+						<td class="innerpadding borderbottom" style="padding: 30px 30px 30px 30px; border-bottom: 1px solid #f2eeed;">
+							<table align="left" border="0" cellpadding="0" cellspacing="0" width="115">
+								<tr>
+									<td height="115" style="padding: 0 20px 20px 0;"><img alt="" border="0" class="fix" height="115" src="http://clinbioinfosspa.es/events/gatk/images/article1.png" style="height: auto;" width="115"></td>
+								</tr>
+							</table><!--[if (gte mso 9)|(IE)]>
+            <table width="380" align="left" cellpadding="0" cellspacing="0" border="0">
+              <tr>
+                <td>
+          <![endif]-->
+							<table align="left" border="0" cellpadding="0" cellspacing="0" class="col380" style="width: 100%; max-width: 380px;">
+								<tr>
+									<td>
+										<table border="0" cellpadding="0" cellspacing="0" width="100%">
+											<tr>
+												<td class="bodycopy" style="color: #153643; font-family: sans-serif; font-size: 16px; line-height: 22px;">Please, in order to prepare an invoice we need you to provide us with your <b>tax information:</b></td>
+											</tr>
+											<tr>
+												<td style="padding: 20px 0 0 0;">
+													<table border="0" cellpadding="0" cellspacing="0" width="100%">
+														<tr>
+															<td class="bodycopy" style="color: #153643; font-family: sans-serif; font-size: 16px; line-height: 22px;">
+																<p><b>Full name</b></p>
+																<p><b>NIE / CIF</b></p>
+																<p><b>Organization, institution.</b></p>
+																<p><b>Billing Address</b></p>
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</table>
+									</td>
+								</tr>
+							</table><!--[if (gte mso 9)|(IE)]>
+                </td>
+              </tr>
+          </table>
+          <![endif]-->
+						</td>
+					</tr>
+					<tr>
+						<td class="innerpadding borderbottom" style="padding: 30px 30px 30px 30px; border-bottom: 1px solid #f2eeed;"><img alt="" border="0" class="fix" src="http://clinbioinfosspa.es/events/gatk/images/GRANADA-gatk.png" style="height: auto;" width="100%"></td>
+					</tr>
+					<tr>
+						<td class="innerpadding bodycopy" style="padding: 30px 30px 30px 30px; color: #153643; font-family: sans-serif; font-size: 16px; line-height: 22px;">
+							In order to address the invoice <b>to your institution</b>, please indicate the detailed information for your billing, otherwise we will address the invoice to you.<br>
+							<br>
+							Finally, you need to make the deposit and send the proof of income to the following email:<br>
+							<b>secretariatecnica-clinbioinfosspa.fps@juntadeandalucia.es</b>
+							<p></p>
+							<p><i>You have chosen the following reservation in your registration:</i></p>
+							<ul>
+								<li><b>Full name:</b> {{ $name }}.</li>
+								<li><b>Position:</b> {{ $institution }}.</li>
+								<li><b>Organization, institution:</b> {{ $position }}.</li>
+								<li><b>Reservation plan:</b> {{ $result }}.</li>
+							</ul>
+							<p>If you have checked the box of the reservation of the visit to the Alhambra, you should keep in mind that it is <b>only a reservation.</b></p>
+							<p>The visit of the Alhambra <b>must be paid in the location of the course</b>, it's not possible to include the fee in the transference.</p><br>
+							<h3>Payment via Bank transfer</h3>
+							<h4>Bank Account Details</h4>
+							<h4></h4>
+							<p><b>Account holder:</b> Fundación Progreso y Salud</p>
+							<p><b>IBAN:</b> ES1101825566790011503217</p>
+							<p><b>BIC/SWIFT:</b> BBVAESMMXXX</p>
+							<p><b>Bank:</b> BBVA</p>
+							<p>PLEASE MENTION YOUR NAME FOLLOWED BY "GATK Workshop 2019" IN YOUR PAYMENT REFERENCE.</p><br>
+							<p>Once the payment has been registered in our system you will receive a confirmation email.</p><br>
+						</td>
+					</tr>
+					<tr>
+						<td bgcolor="#44525F" class="footer" style="padding: 20px 30px 15px 30px;">
+							<table border="0" cellpadding="0" cellspacing="0" width="100%">
+								<tr>
+									<td align="center" class="footercopy" style="font-family: sans-serif; font-size: 14px; color: #ffffff;">
+										&reg; <a href="http://www.clinbioinfosspa.es/" style="text-decoration: none; color: #ffffff">Clinical Bioinformatics Area</a>, <a href="http://www.juntadeandalucia.es/fundacionprogresoysalud/es/home" style="text-decoration: none; color: #ffffff">FPS</a> © <a href="https://github.com/onit4ku/" style="text-decoration: none; color: #ffffff">Rubén García Serrano</a>, 2019
+									</td>
+								</tr>
+								<tr>
+									<td align="center" style="padding: 20px 0 0 0;">
+										<table border="0" cellpadding="0" cellspacing="0">
+											<tr>
+												<td style="text-align: center; padding: 0 10px 0 10px;" width="37">
+													<a href="http://www.clinbioinfosspa.es/"><img alt="CBA" border="0" height="37" src="http://clinbioinfosspa.es/files/image/fps-blanco.svg" style="height: auto;" width="37"></a>
+												</td>
+												<td style="text-align: center; padding: 0 10px 0 10px;" width="37">
+													<a href="https://software.broadinstitute.org/gatk/events/"><img alt="Facebook" border="0" height="37" src="https://avatars2.githubusercontent.com/u/32406064" style="height: auto;" width="37"></a>
+												</td>
+												<td style="text-align: center; padding: 0 10px 0 10px;" width="37">
+													<a href="https://twitter.com/ClinicalBioinfo"><img alt="Twitter" border="0" height="37" src="http://clinbioinfosspa.es/events/gatk/images/twitter.png" style="height: auto;" width="37"></a>
+												</td>
+											</tr>
+										</table>
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table><!--[if (gte mso 9)|(IE)]>
+          </td>
+        </tr>
+    </table>
+    <![endif]-->
+			</td>
+		</tr>
+	</table>
 </body>
 </html>
