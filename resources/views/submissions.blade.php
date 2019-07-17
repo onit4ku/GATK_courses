92bf5ace71174cf2caee619db1937898040e348d
Rubén García Serrano <onitaku@gmail.com>
2019-07-17 15:45:43 +0200

    htaccess

--- resources/views/submissions.blade.php
+++ resources/views/submissions.blade.php
@@ -21,14 +21,9 @@
                     <table class="table table-bordered table-hover">
                         <thead>
                             <tr>
-                                <!-- <th>ID</th>
-                                <th>commandLine</th>
-                                <th>GATKfamiliar</th>
-                                <th>analyzeData</th> -->
                                 <th>Full Name</th>
                                 <th>Institution</th>
                                 <th>Position</th>
-                                <!-- <th>email</th> -->
                                 <th>Plan</th>
                                 <th>Alhambra</th>
                                 <th>Dietary Req.</th>
@@ -46,20 +41,17 @@
         const search = document.getElementById('search');
         const tableBody = document.getElementById('tbody');
         function getContent(){
-
-        const searchValue = search.value;
-
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET','{{route('search')}}/?search=' + searchValue ,true);
-            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-            xhr.onreadystatechange = function() {
-
-                if(xhr.readyState == 4 && xhr.status == 200)
-                {
-                    tableBody.innerHTML = xhr.responseText;
+            const searchValue = search.value;
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET','{{route('search')}}/?search=' + searchValue ,true);
+                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                xhr.onreadystatechange = function() {
+                    if(xhr.readyState == 4 && xhr.status == 200)
+                    {
+                        tableBody.innerHTML = xhr.responseText;
+                    }
                 }
-            }
-            xhr.send()
+                xhr.send()
         }
         search.addEventListener('input',getContent);
     </script>
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group.
-|
- */
-
 Route::get('/upload', 'UploadController@uploadForm');
 Route::post('/upload', 'UploadController@uploadSubmit');
 
-Route::get('/listadoGATK19','SubmissionsController@vista');
-Route::get('/Search','SubmissionsController@search')->name('search');
+Route::get('/listadoGATK19', 'SubmissionsController@vista');
+Route::get('/Search', 'SubmissionsController@search')->name('search');
 
 Route::get('/', function () {
     // return view('registerclosed');
