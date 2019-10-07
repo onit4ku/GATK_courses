795ba318db8ed0dc4596a1063c0eb55734bccff3
Rubén García Serrano <onitaku@gmail.com>
2019-10-07 13:58:40 +0200

    33 list

--- app/Http/Controllers/SubmissionsController.php
+++ app/Http/Controllers/SubmissionsController.php
@@ -18,28 +18,28 @@ class SubmissionsController extends Controller
         $count = GATK::count();
         $output = "";
         // método previo de búsqueda
-                // if ($request->ajax()) {
-            // $submissions = GATK::where('fullName', 'LIKE', '%' . $request->search . "%")->get();
-            $submissions = GATK::get();
-            
-            if ($submissions) {
-                foreach ($submissions as $submission) {
-                    $output .= '<tr>' .
+        // if ($request->ajax()) {
+        // $submissions = GATK::where('fullName', 'LIKE', '%' . $request->search . "%")->get();
+        $submissions = GATK::get();
+
+        if ($submissions) {
+            foreach ($submissions as $submission) {
+                $output .= 
+                '<tr>' .
                     '<td>' . $submission->fullName . '</td>' .
                     '<td>' . $submission->institution . '</td>' .
                     '<td>' . $submission->position . '</td>' .
                     '<td>' . $submission->inputDinner . '</td>' .
                     '<td>' . $submission->inputAlhambra . '</td>' .
                     '<td>' . $submission->dietaryReq . '</td>' .
-                    // '<td>' . $submission->payment . '</td>' .
                     '<td>' . $submission->waitingList . '</td>' .
                     '<td>' . $submission->email . '</td>' .
-                    '</tr>';
-                }
-                
-                echo '<th colspan="8" style="color:black; background-color:#f1f1f1">Total submissions: ' . $count . '</th>';
-                return $output;
+                '</tr>';
             }
+
+            echo '<th colspan="8" style="color:black; background-color:#f1f1f1">Total submissions: ' . $count . '</th>';
+            return $output;
         }
     }
+}
 // }
