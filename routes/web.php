b81a188dda656176f33fe2fdb4dae7ea7f3c0be0
Rubén García Serrano <onitaku@gmail.com>
2019-10-10 13:14:55 +0200

    register closed

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ Route::get('/listadoGATK19', 'SubmissionsController@vista');
 Route::get('search', 'SubmissionsController@search')->name('search');
 
 Route::get('/', function () {
-    // return view('registerclosed');
-    return view('upload_form');
+    return view('registerclosed');
+    // return view('upload_form');
 });
 
 //  vue test
